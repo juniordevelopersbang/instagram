a87d2dc84bc08858fcc18e22f02dbd3056ecde49
juniordevelopersbang <56787096+juniordevelopersbang@users.noreply.github.com>
2020-04-17 21:10:12 +0900

    로그인 페이지 만들기 시작 좋아요 기능 완성

--- main.py
+++ main.py
@@ -13,17 +13,19 @@ db = client.dbsparta
 def home():
     return render_template('mainpage.html')
 
+@app.route('/login')
+def login():
+    return render_template('login.html')
 
 @app.route('/post', methods=["GET"])
 def postpage():
     return render_template('index.html')
 
 
-
 @app.route('/comments', methods=['GET'])
 def listing():
     result = list(db.Instagram.find({}, {'_id': 0}))
-    return jsonify({'result': 'success', 'comments': result})
+    return jsonify({'result': 'success', 'instagram': result})
 
 
 @app.route('/comments', methods=['POST'])
@@ -33,14 +35,17 @@ def saving():
     comment_receive = request.form['comment_give']
     comments = {'comment': comment_receive}
 
-    db.Instagram.insert_one(names, comments)
+    db.Instagram.insert_one(names)
+    db.Instagram.insert_one(comments)
 
     return jsonify({'result': 'success'})
+
 
 @app.route('/')
 def like():
 
     return jsonify({'result': 'success'})
+
 
 if __name__ == '__main__':
     app.run('127.0.0.1', port=5000, debug=True)
