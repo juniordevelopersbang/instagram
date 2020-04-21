31b0153ee1f33d53868fc78fdfab56e3fcf5a27a
juniordevelopersbang <56787096+juniordevelopersbang@users.noreply.github.com>
2020-04-22 00:24:54 +0900

    post append api 만드는 중

--- main.py
+++ main.py
@@ -13,28 +13,47 @@ app = Flask(__name__)
 client = MongoClient('localhost', 27017)
 db = client.dbsparta
 
-
-@app.route('/')
+# mainpage
+@app.route('/mainpage')
 def home():
     return render_template('mainpage.html')
 
-
+# login page
 @app.route('/login')
 def login():
     return render_template('login.html')
 
 
-@app.route('/post', methods=["GET"])
-def postpage():
+@app.route('/post')
+def posting():
     return render_template('post.html')
 
 
+# post 페이지 data 주는 부분
+@app.route('/post', methods=['POST'])
+def postpage():
+    post_recieve = request.form['post_give']
+    posts = {'post': post_recieve}
+    db.instapost.insert_one(posts)
+    return jsonify({'success': True})
+    
+
+
+@app.route('/post/api', methods=['GET'])
+def postlisting():
+    postresult = list(db.instapost.find({}, {'_id': 0}))
+    return jsonify({'result': 'success', 'instapost': postresult})
+    
+
+
+# comments page db 조회
 @app.route('/comments', methods=['GET'])
 def listing():
     result = list(db.Instagram.find({}, {'_id': 0}))
     return jsonify({'result': 'success', 'instagram': result})
 
 
+# 작성받은 comments db에 집어넣기
 @app.route('/comments', methods=['POST'])
 def saving():
     comment_receive = request.form['comment_give']
@@ -45,6 +64,7 @@ def saving():
     return jsonify({'result': 'success'})
 
 
+# 파일 업로드
 @app.route('/uploader', methods = ['GET', 'POST'])
 def upload_file():
    if request.method == 'POST':
