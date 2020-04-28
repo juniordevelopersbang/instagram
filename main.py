fc5f9aeac37bdc6d4f39c488a12bd0b888a0297a
juniordevelopersbang <56787096+juniordevelopersbang@users.noreply.github.com>
2020-04-28 17:04:25 +0900

    making postid

--- main.py
+++ main.py
@@ -4,75 +4,91 @@ from flask import Flask, render_template, jsonify, request
 from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
-client = MongoClient('localhost', 27017)
+client = MongoClient("localhost", 27017)
 db = client.dbsparta
 
 # mainpage
-@app.route('/mainpage')
+@app.route("/mainpage")
 def home():
-    return render_template('mainpage.html')
+    return render_template("mainpage.html")
+
 
 # login page
-@app.route('/login')
+@app.route("/login")
 def loginhtml():
-    return render_template('login.html')
+    return render_template("login.html")
+
 
 # signup page
-@app.route('/signup')
+@app.route("/signup")
 def signup():
-    return render_template('signup.html')
+    return render_template("signup.html")
 
 
 # post page
-@app.route('/post')
+@app.route("/post")
 def posting():
-    return render_template('post.html')
+    return render_template("post.html")
 
 
 # post 페이지 data 주는 부분
-@app.route('/post', methods=['POST'])
+@app.route("/post", methods=["POST"])
 def postpage():
-    post_recieve = request.form['post_give']
-    posts = {'post': post_recieve}
+    post_recieve = request.form["post_give"]
+    posts = {"postcontent": post_recieve}
     db.instapost.insert_one(posts)
-    db.instapost.update({'post': 'hi'}, {'$inc': {'count': 1}}, upsert=False)
-    return jsonify({'success': True})
+    return jsonify({"success": True})
 
 
 # post page db 데이터 조회
-@app.route('/post/api', methods=['GET'])
+@app.route("/post/api", methods=["GET"])
 def postlisting():
-    postresult = list(db.instapost.find({}, {'_id': 0}))
-    return jsonify({'result': 'success', 'instapost': postresult})
+    postresult = list(db.instapost.find({}))
+    return jsonify({"result": "success", "instapost": postresult})
 
 
 # comments page db 조회
-@app.route('/comments', methods=['GET'])
+@app.route("/comments", methods=["GET"])
 def listing():
-    result = list(db.Instagram.find({}, {'_id': 0}))
-    return jsonify({'result': 'success', 'instagram': result})
+    result = list(db.Instagram.find({}, {"_id": 0}))
+    print(result)
+    postresult = list(db.instapost.find({}))
+    postid = (item for item in result if item["postid"] == 1)
+    dict = next(postid, False)
+    for i in postresult:
+        db.instapost.find({'_id': i})
+        if i == dict["postid"]:
+            return({'success': True})
+    return jsonify({'success': True, 'instagram': dict})
+    
 
 
 # 작성받은 comments db에 집어넣기
-@app.route('/comments', methods=['POST'])
+@app.route("/comments", methods=["POST"])
 def saving():
-    comment_receive = request.form['comment_give']
-    comments = {'comment': comment_receive}
-    if comments['comment'] == '':
-        return jsonify({'result': 'fail'})
+    comment_receive = request.form["comment_give"]
+    comments = {"comment": comment_receive}
+    
+    postid_receive = request.form["postid_give"]
+    postids = {"postid": postid_receive}
+    
+    if comments["comment"] == "":
+        return jsonify({"result": "fail"})
     else:
         db.Instagram.insert_one(comments)
-    return jsonify({'result': 'success'})
+        db.Instagram.insert_one(postids)
+        
+    return jsonify({"result": "success"})
 
 
 # 파일 업로드
-@app.route('/uploader', methods=['GET', 'POST'])
+@app.route("/uploader", methods=["GET", "POST"])
 def upload_file():
-    if request.method == 'POST':
-        f = request.files['file']
-        f.save('./uploadimg/' + secure_filename(f.filename))
-        return 'file uploaded successfully'
+    if request.method == "POST":
+        f = request.files["file"]
+        f.save("./uploadimg/" + secure_filename(f.filename))
+        return "file uploaded successfully"
 
 
-if __name__ == '__main__':
-    app.run('127.0.0.1', port=5000, debug=True)
+if __name__ == "__main__":
+    app.run("127.0.0.1", port=5000, debug=True)
